0a720b547529a65d54ea184c086973807cea1f67
Neal van Veen <nvanveen@magnius.com>
2020-05-10 02:03:23 +0200

    replace internal transform with babel
    Using the webpack module of the theme file wasn't enough in that
    create-react-app by default uses ES modules, which can't be
    evaluated in a Node VM to retrieve the default export.

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,12 @@
-import vm from 'vm';
+/* eslint-disable global-require */
+/* eslint-disable no-unused-vars */
 import path from 'path';
-// eslint-disable-next-line no-unused-vars
+import fs from 'fs';
+import vm from 'vm';
 import { ExtendedLoaderContext } from 'loader-runner';
+import * as babel from '@babel/core';
 
-const toCamelCase = (s: string) =>
+const toKebabCase = (s: string) =>
   s.replace(/([a-z])([A-Z])/g, '$1-$2').toLowerCase();
 
 declare module 'loader-runner' {
@@ -16,58 +19,83 @@ declare module 'loader-runner' {
   }
 }
 
-function loader(
+async function loader(
   this: ExtendedLoaderContext,
   source: string,
   map: unknown,
   meta: unknown
 ) {
-  if (!this.resourceQuery) {
-    throw new Error('No resource query available in loader');
-  }
   if (!this.async) throw new Error('No async avaiable in loader');
-  if (!this.loadModule) throw new Error('No module loader available in loader');
-  if (!this.context) throw new Error('Invalid context path in loader');
-
   const callback = this.async() as (
     err?: Error | null,
     source?: string,
     map?: unknown,
     meta?: unknown
   ) => void;
-  if (!callback) throw new Error('No callback available in loader');
 
-  const m = this.resourceQuery.match(/\?theme=(.*)$/);
-  if (m?.length !== 2) throw new Error('Invalid theme file passed to loader');
-  // @ts-ignore
-  const themePath = path.resolve(this.context, m[1]);
-  this.addDependency(themePath);
+  try {
+    if (!callback) throw new Error('No callback available in loader');
+    if (!this.resourceQuery) {
+      throw new Error('No resource query available in loader');
+    }
+    if (!this.loadModule) {
+      throw new Error('No module loader available in loader');
+    }
+    if (!this.context) throw new Error('Invalid context path in loader');
 
-  this.loadModule(themePath, (err: Error, themeSource: string) => {
-    if (err) return callback(err);
-    console.log('test', themeSource);
-    const context: {
-      exports: { default: Record<string, string> | undefined };
-    } = { exports: { default: undefined } };
-    const script = new vm.Script(themeSource);
+    const m = this.resourceQuery.match(/\?theme=(.*)$/);
+    if (m?.length !== 2) throw new Error('Invalid theme file passed to loader');
+    const themePath = path.resolve(this.context, m[1]);
+    const themeSrc = await fs.promises.readFile(themePath, 'utf-8');
+
+    const es = await new Promise<babel.BabelFileResult | null>(
+      (resolve, reject) => {
+        babel.transform(
+          themeSrc,
+          {
+            filename: themePath,
+            babelrc: false,
+            presets: [
+              require('@babel/preset-typescript'),
+              require('@babel/preset-env'),
+            ],
+          },
+          (err, src) => {
+            if (err) reject(err);
+            resolve(src);
+          }
+        );
+      }
+    );
+    if (!es?.code) throw new Error(`Unable to compile ${themePath}`);
+    const context = vm.createContext({ exports: {} });
+    const script = new vm.Script(es.code);
     script.runInNewContext(context);
+
     const {
       exports: { default: theme },
     } = context;
-    if (!theme) return callback(new Error('Invalid theme exported in loader'));
+
     const sass = Object.keys(theme)
-      .map((k) => `$${toCamelCase(k)}: ${theme[k]};`)
+      .map((k) => `$${toKebabCase(k)}: ${theme[k]};`)
       .join('\n');
-    callback(null, `${sass}\n${source}`, map, meta);
-  });
+
+    return callback(null, `${sass}\n${source}`, map, meta);
+  } catch (err) {
+    callback(err);
+  }
+
+  return source;
 }
 
 function pitch(this: ExtendedLoaderContext) {
   const [first, ...otherLoaders] = this.loaders;
   if (first.path !== require.resolve('./')) return;
-  this.loaders = [...otherLoaders, first];
+  this.loaders = otherLoaders;
+  this.loaders.push(first);
 }
 
 loader.pitch = pitch;
 
+// @ts-ignore
 export = loader;
